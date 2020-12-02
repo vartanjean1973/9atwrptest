399d791c83f31a8982477f2977be953a0490ce84
darkkingredian <46134036+darkkingredian@users.noreply.github.com>
2020-12-02 14:31:52 +0600

    add pb config

--- omni_dandelion.mk
+++ omni_dandelion.mk
@@ -28,6 +28,9 @@ $(call inherit-product, device/xiaomi/dandelion/device.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
+# Inherit from our pitch black product configuration
+$(call inherit-product, vendor/pb/config/common.mk)
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := dandelion
 PRODUCT_NAME := omni_dandelion
